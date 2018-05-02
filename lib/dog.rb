35cb6e2c26b60cdb153584b88812ef66743ae662
Joseph Parker <jparker010190@gmail.com>
2018-05-02 03:08:45 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,8 +3,12 @@ class Dog
   attr_reader :id
   def initialize(id=nil, name, breed)
     @id = id
-    @name = name 
+    @name = name
     @breed = breed
     include Enumerable
+  end
+
+  def self.create_table
+    sql = "CREATE TABLE IF NOT EXISTS dogs (id INTEGER PRIMARY KEY, name TEXT, breed TEXT)"
   end
 end
