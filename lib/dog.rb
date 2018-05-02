9e528c101033cdd5c7d1a6824192e9b5c628bc21
Joseph Parker <jparker010190@gmail.com>
2018-05-02 03:21:37 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -24,8 +24,9 @@ class Dog
   end
 
   def self.find_by_name(name)
-    sql = "SELECT * FROM dogs where name = ?"
-    DB[:conn].execute(sql, self.name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Student.new(result[0], result[1], result[2])
   end
 
   def save
@@ -37,6 +38,14 @@ class Dog
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() from dogs")[0][0]
+    end
   end
-end
+  
+  def update
+    sql = <<-SQL
+    UPDATE dogs SET name = ?, breed = ? where id =?
+    SQL
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+  
 end
